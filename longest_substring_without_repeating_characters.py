4e94cf408c0ce051564e77caa64a5cc2516b12b8
Kywll <mkywll74@gmail.com>
2025-12-13 16:05:39 +0800

    better solution

--- longest_substring_without_repeating_characters.py
+++ longest_substring_without_repeating_characters.py
@@ -52,3 +52,64 @@ g = Solution()
 s = "pwwkew"
 
 print(g.lengthOfLongestSubstring(s))
+
+
+
+
+
+'''
+Optimized and proper sliding window solition:
+
+Thought Process:
+I used a proper sliding window approach, and used a set to determine if there are any duplicates.
+I first checked if the right pointer has a duplicate already in the set, which I then checked in
+a loop if the left pointer is equals to the right or not, if it is, I just removed the left pointer
+because it's no longer part of the window, then at the end I just moved the left pointer to the right
+once it's equals to the right pointer and didn't need to remove the left pointer because the right 
+pointer is equals to it and should be included in the set. If in case that the right pointer is not 
+a duplicate, I just add it to the set. At the end of each iteration, I just update the longest 
+substring based on the length of the set.
+
+
+Complexity:
+O(n) time
+O(m) space
+
+What I learned:
+I learned that I could use a nested loop using a sliding window technique and still get a linear
+time as long as the pointers are moving in one direction. I also learned that a set is useful
+when I simply want to check if a duplicate already exists without the need for extra space for
+keys.
+
+
+'''
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        if len(s) == 0:
+            return 0
+
+        longest = 1
+        charSet = set()
+        charSet.add(s[0])
+
+        l = 0
+        for r in range(1, len(s)):
+            if s[r] in charSet:
+                while s[l] != s[r]:
+                    charSet.remove(s[l])
+                    l+=1
+                l+=1
+            else:
+                charSet.add(s[r])
+            longest = max(longest, len(charSet))
+
+        return longest
+
+
+g = Solution()
+
+s = "abba"
+
+print(g.lengthOfLongestSubstring(s))
+
